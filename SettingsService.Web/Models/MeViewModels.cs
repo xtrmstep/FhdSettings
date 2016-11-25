ece6a3272f7fb2a30acb1926faa9a1e8cd401a51
Alexander Goida <alexander.goida@gmail.com>
2016-11-25 19:03:58 +0200

    Removed Owin related assemblies and files

--- SettingsService.Web/Models/MeViewModels.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SettingsService.Web.Models
-{
-    // Models returned by MeController actions.
-    public class GetViewModel
-    {
-        public string Hometown { get; set; }
-    }
-}
