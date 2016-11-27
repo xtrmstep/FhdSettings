10be210ef8d21ca27dfeb41d1660ef4d0e9504a7
Alexander Goida <alexander.goida@gmail.com>
2016-11-27 17:46:41 +0200

    Addde proxy interface and implementation for Settings API

---
+++ SettingsService.Web/Types/ISettingsApiProxy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SettingsService.Web.Types
+{
+    public interface ISettingsApiProxy
+    {
+    }
+}
---
+++ SettingsService.Web/Types/SettingsApiProxy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SettingsService.Web.Types
+{
+    public class SettingsApiProxy : ISettingsApiProxy
+    {
+    }
+}
