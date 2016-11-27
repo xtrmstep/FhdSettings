51776a652839e962fb2b43141ab777623d2c1f21
Alexander Goida <alexander.goida@gmail.com>
2016-11-27 17:49:33 +0200

    Added comment to the class

--- SettingsService.Web/Types/SettingsApiProxy.cs
+++ SettingsService.Web/Types/SettingsApiProxy.cs
@@ -5,6 +5,12 @@ using System.Web;
 
 namespace SettingsService.Web.Types
 {
+    /// <summary>
+    /// Implementation of the proxy to Settings API
+    /// </summary>
+    /// <remarks>
+    /// Read more https://www.asp.net/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+    /// </remarks>
     public class SettingsApiProxy : ISettingsApiProxy
     {
     }
